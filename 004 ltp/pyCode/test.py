2439e8bc037646b259a5abbe52705c8214baa31d
Xinlang <516322163@qq.com>
2018-05-19 17:48:17 +0800

    1.ltp 的 python 测试文件

---
+++ 004 ltp/pyCode/test.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from pyltp import SentenceSplitter as ss
+from pyltp import Segmentor
+import os
+
+
+def split_sentence(sentences):
+    """分句"""
+    sentence = ss.split(sentences)
+    print('\n'.join(sentence))
+
+
+def split_word(sentence):
+    """分词"""
+    segmentor = Segmentor()
+    segmentor.load("../ltp/cws.model")
+    words = segmentor.segment(sentence)
+    # print('\n'.join(words))
+    segmentor.release()
+    # print(words)
+
+    return words
+
+
+def recognize_word(words):
+    """命名实体识别"""
+    from pyltp import NamedEntityRecognizer
+    recognizer = NamedEntityRecognizer()
+    recognizer.load("../ltp/ner.model")
+
+    postags = ['nh', 'r', 'r', 'v']
+    netags = recognizer.recognize(['元芳', '你', '怎么', '看'], postags)
+    print('\t'.join(netags))
+    recognizer.release()
+
+
+if __name__ == "__main__":
+    words = split_word("中国位于亚洲东部")
+    list_word = list(words)
+    print(list_word)
+    recognize_word(list_word)
+    # split_word('辛浪在金蝶云之家工作')
+    # split_sentence('元芳你怎么看？我就趴窗口上看呗！')
+
