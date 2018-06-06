592534989e8cce8bc62f69c7a3d666e9eb915447
EricXin <516322163@qq.com>
2018-06-06 23:36:02 +0800

    加入明细数据

--- 004 ltp/pyCode/unique_data.py
+++ 004 ltp/pyCode/unique_data.py
@@ -4,7 +4,41 @@ import re
 import io
 
 
-def uniqe_data(file_path):
+def take_length(data):
+    return len(data.split(" "))
+
+
+def line_unique(file_path):
+    """
+    file_path文件的每一行都不相同
+    :param file_path:
+    :return:
+    """
+    if not os.path.exists(file_path):
+        print(file_path + " 不存在")
+        return
+
+    f_in = open(file_path, mode="r", encoding="utf-8")
+    line = f_in.readline()
+    lines = []
+    while line:
+        # print(line)
+        lines.append(line.strip())
+        line = f_in.readline()
+    f_in.close()
+
+    lines = list(set(lines))
+    print("lines: %d" % len(lines))
+    print(lines)
+    lines.sort(key=take_length)
+    print(lines)
+    f_out = open(file_path + ".line_unique", mode="w", encoding="utf-8")
+    for line in lines:
+        f_out.write(line + "\n")
+    f_out.close()
+
+
+def word_unique(file_path):
     """
     对 file_path 文件的名词进行处理，保证没有重复的名词
     :param file_path:
@@ -17,7 +51,7 @@ def uniqe_data(file_path):
     f_in = open(file_path, mode="r", encoding="UTF-8")
     line_in = f_in.readline()
 
-    pattern = re.compile('([^/]+)\S+')
+    pattern = re.compile('(\S+)')
     words = []
     while line_in:
         result = pattern.findall(line_in)
@@ -25,7 +59,7 @@ def uniqe_data(file_path):
             word = word.strip()
             if len(word) > 1:
                 words.append(word)
-            
+
         line_in = f_in.read()
         words = list(set(words))
 
@@ -40,4 +74,5 @@ def uniqe_data(file_path):
 
 
 if __name__ == "__main__":
-    uniqe_data("./data/destination.data.out")
+    line_unique("./data/train.data.out")
+    # word_unique("./data/train.data.out")
